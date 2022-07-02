373559703cc7f0ea16b5476af5b8e1593d519b4b
Nweke273 <chisom5710@gmail.com>
2022-07-02 10:47:33 +0100

    little fix in the route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
                 <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
                     <a class="no-underline hover:underline" href="/">Home</a>
-                    <a class="no-underline hover:underline" href="/blog">Blog</a>
+                    <a class="no-underline hover:underline" href="/all-posts">Blog</a>
                     @guest
                     <a class="no-underline hover:underline" href="{{ route('user.login') }}">Login</a>
                     @if (Route::has('register'))
