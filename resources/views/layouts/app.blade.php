f8365ed2c653b4398ab78800fc11b64ba1b64287
Nweke273 <chisom5710@gmail.com>
2022-07-02 10:19:56 +0100

    changed some style in the app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,12 +21,12 @@
     <div id="app">
         <header class="bg-gray-800 py-6">
             <div class="container mx-auto flex justify-between items-center px-6">
-                <div>
-                    @guest
-                    <a class="no-underline hover:underline text-white" href="{{ route('admin.login') }}">
-                        {{ __('Admin Login') }}</a>
-                    @endguest
-                </div>
+
+                @guest
+                <a class="no-underline hover:underline text-white" href="{{ route('admin.login') }}">
+                    Admin Login</a>
+                @endguest
+
                 <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
                     <a class="no-underline hover:underline" href="/">Home</a>
                     <a class="no-underline hover:underline" href="/blog">Blog</a>
